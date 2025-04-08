00c7d810517361f738df44e88cdfb45d2facf5d1
unknown <haider2shah@yahoo.com>
2025-04-07 17:26:57 -0700

    Updated navigation bar without the button

---
+++ src/app/components/navBar.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import navStyles from '../styles_css/navigationbar.module.css';
+
+const NavBar = () => {
+  return (
+    <nav className={navStyles.navbar}>
+      <ul className={navStyles['nav-links']}>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About us</Link></li>
+
+    <div className = {navStyles.logo}>Pinnacle Studios</div>
+
+        <li><Link href="/services">Services</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
